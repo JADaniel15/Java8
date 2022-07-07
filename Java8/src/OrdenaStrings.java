88b0dd9d66c636e22a90b592d67099aa30eac64c
João Augusto <j.asoul@hotmail.com>
2022-07-07 01:22:03 -0300

    alteracoes Default

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
@@ -11,10 +12,25 @@ public class OrdenaStrings {
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
 		
-		Collections.sort(palavras);
+		Comparator<String> comparador = new ComparadorPorTamanho();
+		//Collections.sort(palavras, comparador);
+		palavras.sort(comparador);
 		System.out.println(palavras);
 		
 
 	}
 
+}
+class ComparadorPorTamanho implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+		if(s1.length()< s2.length())
+			return -1;
+		if(s1.length()> s2.length())
+			return 1;
+		return 0;
+	}
+	
+	
 }
