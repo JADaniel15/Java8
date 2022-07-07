5f62e088989e6c3fff9ef7cb78c2f5d669b961cb
João Augusto <j.asoul@hotmail.com>
2022-07-07 01:33:05 -0300

    alteracao e atualizacao

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
-import java.util.Collections;
+//import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
@@ -17,9 +18,26 @@ public class OrdenaStrings {
 		palavras.sort(comparador);
 		System.out.println(palavras);
 		
+		//for (String p : palavras) {
+			//System.out.println(p);
+		//}
+		
+		Consumer<String>consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
+		
 
 	}
 
+}
+
+class ImprimeNaLinha implements Consumer<String>{
+
+	@Override
+	public void accept(String s) {
+		
+		System.out.println(s);
+	}
+	
 }
 class ComparadorPorTamanho implements Comparator<String>{
 
